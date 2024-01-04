7318a917c87bd7a50f72516106dddc24645f01f0
Tobias HÃ¼fner <tobias.huefner@biophys.mpg.de>
2024-01-04 14:49:50 +0100

    Bug fix in verbose mode

--- BayesTyper/ray_tools.py
+++ BayesTyper/ray_tools.py
@@ -5,6 +5,7 @@ def retrieve_failed_workers(worker_id_list, verbose=False):
 
     failed_keywords = [
         "fail",
+        "failed",
         "error",
         "nil",
         "pending_args_avail",
@@ -29,40 +30,63 @@ def retrieve_failed_workers(worker_id_list, verbose=False):
             failed = True
         elif isinstance(s, list):
             for _s in s:
+                if failed:
+                    break
                 try:
-                    _state =_s.state.lower()
+                    _state =_s.state.lower().rstrip().lstrip()
                 except:
                     failed = True
                     break
                 for k in failed_keywords:
                     if k in _state:
                         failed = True
+                        if verbose:
+                            print(
+                                f"Failed due to {_state}"
+                                )
                         break
 
-                if verbose:
+                if verbose and not failed:
+                    _found = False
                     for k in succes_keywords:
-                        if k not in _state:
-                            print(
-                                "WHAT IS THIS??????????",
-                                _state
-                            )
+                        if k in _state:
+                            _found = True
+                            break
+                    if not _found:
+                        print(
+                            "WHAT IS THIS??????????",
+                            _state,
+                        )
         else:
             try:
-                _state = s.state.lower()
+                _state = s.state.lower().rstrip().lstrip()
                 for k in failed_keywords:
                     if k in _state:
                         failed = True
+                        if verbose:
+                            print(
+                                f"Failed due to {_state}"
+                                )
                         break
             except:
                 failed = True
 
-            if verbose:
+            if verbose and not failed:
+                _found = False
                 for k in succes_keywords:
-                    if k not in _state:
-                        print(
-                            "WHAT IS THIS??????????",
-                            _state
-                        )
+                    if k in _state:
+                        if verbose:
+                            print(
+                                f"Failed due to {_state}"
+                                )
+                        _found = True
+                        break
+                if not _found:
+                    print(
+                        "WHAT IS THIS??????????",
+                        _state,
+                    )
+
         if failed:
             failed_worker_id_list.append(
                 worker_id
